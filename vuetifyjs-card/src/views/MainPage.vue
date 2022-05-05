1b3bfb456fd2ae36a7bef52070ebd1813098ef7e
photiary <john@funa.com>
2022-05-05 12:15:25 +0900

    채널 선택 추가

--- vuetifyjs-card/src/views/MainPage.vue
+++ vuetifyjs-card/src/views/MainPage.vue
@@ -3,7 +3,38 @@
     <v-main>
       <v-app-bar color="blue" elevation="4">
         <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>mdi-cog-outline</v-icon>
+        </v-btn>
       </v-app-bar>
+      <div>
+        Image
+      </div>
+      <v-sheet
+        class="mx-auto"
+        max-width="700"
+      >
+        <v-slide-group
+          v-model="model"
+          center-active
+        >
+          <v-slide-item
+            v-for="n in 10"
+            :key="n"
+            v-slot="{ active, toggle }"
+          >
+            <v-card
+              :color="active ? 'primary' : 'grey lighten-1'"
+              class="ma-2"
+              height="40"
+              width="40"
+              @click="toggle"
+            >
+            </v-card>
+          </v-slide-item>
+        </v-slide-group>
+      </v-sheet>
       <v-container>
         Hello World
       </v-container>
@@ -26,7 +57,7 @@
     // 컴포넌트 변수 그룹
     data() {
       return {
-
+        model: null,
       };
     },
     computed: {},
